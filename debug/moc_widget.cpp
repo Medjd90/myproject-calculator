9c12157507ffcff54bf8f5eec8d709dc1971f945
Medjd90 <tchequeevent@gmail.com>
2024-02-02 05:30:52 -0500

    commit initial

---
+++ debug/moc_widget.cpp
@@ -0,0 +1,201 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../calculator/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.6.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[23];
+    char stringdata0[415];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 19), // "on_pbefface_clicked"
+QT_MOC_LITERAL(4, 50, 15), // "on_pbun_clicked"
+QT_MOC_LITERAL(5, 66, 17), // "on_pbplus_clicked"
+QT_MOC_LITERAL(6, 84, 18), // "on_Pbegale_clicked"
+QT_MOC_LITERAL(7, 103, 17), // "on_pbdeux_clicked"
+QT_MOC_LITERAL(8, 121, 18), // "on_pbtrois_clicked"
+QT_MOC_LITERAL(9, 140, 18), // "on_pbmoins_clicked"
+QT_MOC_LITERAL(10, 159, 23), // "on_pbmultiplier_clicked"
+QT_MOC_LITERAL(11, 183, 20), // "on_pbdiviser_clicked"
+QT_MOC_LITERAL(12, 204, 19), // "on_pbrecule_clicked"
+QT_MOC_LITERAL(13, 224, 20), // "on_Pbvirgule_clicked"
+QT_MOC_LITERAL(14, 245, 19), // "on_pbsquare_clicked"
+QT_MOC_LITERAL(15, 265, 19), // "on_pbquatre_clicked"
+QT_MOC_LITERAL(16, 285, 17), // "on_pbcinq_clicked"
+QT_MOC_LITERAL(17, 303, 16), // "on_pbsix_clicked"
+QT_MOC_LITERAL(18, 320, 17), // "on_pbsept_clicked"
+QT_MOC_LITERAL(19, 338, 17), // "on_pbhuit_clicked"
+QT_MOC_LITERAL(20, 356, 17), // "on_pbneuf_clicked"
+QT_MOC_LITERAL(21, 374, 22), // "on_Pbplusmoins_clicked"
+QT_MOC_LITERAL(22, 397, 17) // "on_Pbzero_clicked"
+
+    },
+    "Widget\0on_pushButton_clicked\0\0"
+    "on_pbefface_clicked\0on_pbun_clicked\0"
+    "on_pbplus_clicked\0on_Pbegale_clicked\0"
+    "on_pbdeux_clicked\0on_pbtrois_clicked\0"
+    "on_pbmoins_clicked\0on_pbmultiplier_clicked\0"
+    "on_pbdiviser_clicked\0on_pbrecule_clicked\0"
+    "on_Pbvirgule_clicked\0on_pbsquare_clicked\0"
+    "on_pbquatre_clicked\0on_pbcinq_clicked\0"
+    "on_pbsix_clicked\0on_pbsept_clicked\0"
+    "on_pbhuit_clicked\0on_pbneuf_clicked\0"
+    "on_Pbplusmoins_clicked\0on_Pbzero_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  119,    2, 0x08 /* Private */,
+       3,    0,  120,    2, 0x08 /* Private */,
+       4,    0,  121,    2, 0x08 /* Private */,
+       5,    0,  122,    2, 0x08 /* Private */,
+       6,    0,  123,    2, 0x08 /* Private */,
+       7,    0,  124,    2, 0x08 /* Private */,
+       8,    0,  125,    2, 0x08 /* Private */,
+       9,    0,  126,    2, 0x08 /* Private */,
+      10,    0,  127,    2, 0x08 /* Private */,
+      11,    0,  128,    2, 0x08 /* Private */,
+      12,    0,  129,    2, 0x08 /* Private */,
+      13,    0,  130,    2, 0x08 /* Private */,
+      14,    0,  131,    2, 0x08 /* Private */,
+      15,    0,  132,    2, 0x08 /* Private */,
+      16,    0,  133,    2, 0x08 /* Private */,
+      17,    0,  134,    2, 0x08 /* Private */,
+      18,    0,  135,    2, 0x08 /* Private */,
+      19,    0,  136,    2, 0x08 /* Private */,
+      20,    0,  137,    2, 0x08 /* Private */,
+      21,    0,  138,    2, 0x08 /* Private */,
+      22,    0,  139,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Widget *_t = static_cast<Widget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->on_pbefface_clicked(); break;
+        case 2: _t->on_pbun_clicked(); break;
+        case 3: _t->on_pbplus_clicked(); break;
+        case 4: _t->on_Pbegale_clicked(); break;
+        case 5: _t->on_pbdeux_clicked(); break;
+        case 6: _t->on_pbtrois_clicked(); break;
+        case 7: _t->on_pbmoins_clicked(); break;
+        case 8: _t->on_pbmultiplier_clicked(); break;
+        case 9: _t->on_pbdiviser_clicked(); break;
+        case 10: _t->on_pbrecule_clicked(); break;
+        case 11: _t->on_Pbvirgule_clicked(); break;
+        case 12: _t->on_pbsquare_clicked(); break;
+        case 13: _t->on_pbquatre_clicked(); break;
+        case 14: _t->on_pbcinq_clicked(); break;
+        case 15: _t->on_pbsix_clicked(); break;
+        case 16: _t->on_pbsept_clicked(); break;
+        case 17: _t->on_pbhuit_clicked(); break;
+        case 18: _t->on_pbneuf_clicked(); break;
+        case 19: _t->on_Pbplusmoins_clicked(); break;
+        case 20: _t->on_Pbzero_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject Widget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Widget.data,
+      qt_meta_data_Widget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
+        return static_cast<void*>(const_cast< Widget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 21;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ ui_widget.h
@@ -0,0 +1,283 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *res;
+    QGridLayout *gridLayout;
+    QPushButton *pbquatre;
+    QPushButton *pbsquare;
+    QPushButton *pbcinq;
+    QPushButton *pbun;
+    QPushButton *Pbplusmoins;
+    QPushButton *pbsix;
+    QPushButton *Pbvirgule;
+    QPushButton *Pbzero;
+    QPushButton *pbtrois;
+    QPushButton *pbdeux;
+    QPushButton *pbsept;
+    QPushButton *pbhuit;
+    QPushButton *pbrecule;
+    QPushButton *pbneuf;
+    QPushButton *pbefface;
+    QPushButton *pbdiviser;
+    QPushButton *pbmultiplier;
+    QPushButton *pbmoins;
+    QPushButton *pbplus;
+    QPushButton *Pbegale;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QStringLiteral("Widget"));
+        Widget->resize(329, 412);
+        Widget->setMinimumSize(QSize(0, 60));
+        verticalLayoutWidget = new QWidget(Widget);
+        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(70, 50, 182, 276));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        res = new QLabel(verticalLayoutWidget);
+        res->setObjectName(QStringLiteral("res"));
+        res->setMinimumSize(QSize(0, 40));
+        QFont font;
+        font.setPointSize(12);
+        font.setBold(true);
+        font.setWeight(75);
+        res->setFont(font);
+        res->setStyleSheet(QLatin1String("background-color: rgb(194, 194, 194);\n"
+"border-color: rgb(0, 0, 0);"));
+        res->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        horizontalLayout->addWidget(res);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setSpacing(6);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        pbquatre = new QPushButton(verticalLayoutWidget);
+        pbquatre->setObjectName(QStringLiteral("pbquatre"));
+        pbquatre->setMinimumSize(QSize(0, 40));
+        pbquatre->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbquatre, 2, 0, 1, 1);
+
+        pbsquare = new QPushButton(verticalLayoutWidget);
+        pbsquare->setObjectName(QStringLiteral("pbsquare"));
+        pbsquare->setMinimumSize(QSize(0, 40));
+        pbsquare->setMaximumSize(QSize(40, 16777215));
+        pbsquare->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbsquare, 0, 0, 1, 1);
+
+        pbcinq = new QPushButton(verticalLayoutWidget);
+        pbcinq->setObjectName(QStringLiteral("pbcinq"));
+        pbcinq->setMinimumSize(QSize(0, 40));
+        pbcinq->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbcinq, 2, 1, 1, 1);
+
+        pbun = new QPushButton(verticalLayoutWidget);
+        pbun->setObjectName(QStringLiteral("pbun"));
+        pbun->setMinimumSize(QSize(0, 40));
+        pbun->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbun, 3, 0, 1, 1);
+
+        Pbplusmoins = new QPushButton(verticalLayoutWidget);
+        Pbplusmoins->setObjectName(QStringLiteral("Pbplusmoins"));
+        Pbplusmoins->setMinimumSize(QSize(0, 40));
+        Pbplusmoins->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(Pbplusmoins, 4, 0, 1, 1);
+
+        pbsix = new QPushButton(verticalLayoutWidget);
+        pbsix->setObjectName(QStringLiteral("pbsix"));
+        pbsix->setMinimumSize(QSize(0, 40));
+        pbsix->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbsix, 2, 2, 1, 1);
+
+        Pbvirgule = new QPushButton(verticalLayoutWidget);
+        Pbvirgule->setObjectName(QStringLiteral("Pbvirgule"));
+        Pbvirgule->setMinimumSize(QSize(0, 40));
+        Pbvirgule->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(Pbvirgule, 4, 2, 1, 1);
+
+        Pbzero = new QPushButton(verticalLayoutWidget);
+        Pbzero->setObjectName(QStringLiteral("Pbzero"));
+        Pbzero->setMinimumSize(QSize(0, 40));
+        Pbzero->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(Pbzero, 4, 1, 1, 1);
+
+        pbtrois = new QPushButton(verticalLayoutWidget);
+        pbtrois->setObjectName(QStringLiteral("pbtrois"));
+        pbtrois->setMinimumSize(QSize(0, 40));
+        pbtrois->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbtrois, 3, 2, 1, 1);
+
+        pbdeux = new QPushButton(verticalLayoutWidget);
+        pbdeux->setObjectName(QStringLiteral("pbdeux"));
+        pbdeux->setMinimumSize(QSize(0, 40));
+        pbdeux->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbdeux, 3, 1, 1, 1);
+
+        pbsept = new QPushButton(verticalLayoutWidget);
+        pbsept->setObjectName(QStringLiteral("pbsept"));
+        pbsept->setMinimumSize(QSize(0, 40));
+        pbsept->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbsept, 1, 0, 1, 1);
+
+        pbhuit = new QPushButton(verticalLayoutWidget);
+        pbhuit->setObjectName(QStringLiteral("pbhuit"));
+        pbhuit->setMinimumSize(QSize(0, 40));
+        pbhuit->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbhuit, 1, 1, 1, 1);
+
+        pbrecule = new QPushButton(verticalLayoutWidget);
+        pbrecule->setObjectName(QStringLiteral("pbrecule"));
+        pbrecule->setMinimumSize(QSize(0, 40));
+        pbrecule->setMaximumSize(QSize(40, 16777215));
+        pbrecule->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbrecule, 0, 1, 1, 1);
+
+        pbneuf = new QPushButton(verticalLayoutWidget);
+        pbneuf->setObjectName(QStringLiteral("pbneuf"));
+        pbneuf->setMinimumSize(QSize(0, 40));
+        pbneuf->setMaximumSize(QSize(40, 16777215));
+
+        gridLayout->addWidget(pbneuf, 1, 2, 1, 1);
+
+        pbefface = new QPushButton(verticalLayoutWidget);
+        pbefface->setObjectName(QStringLiteral("pbefface"));
+        pbefface->setMinimumSize(QSize(0, 40));
+        pbefface->setMaximumSize(QSize(40, 16777215));
+        pbefface->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbefface, 0, 2, 1, 1);
+
+        pbdiviser = new QPushButton(verticalLayoutWidget);
+        pbdiviser->setObjectName(QStringLiteral("pbdiviser"));
+        pbdiviser->setMinimumSize(QSize(0, 40));
+        pbdiviser->setMaximumSize(QSize(40, 16777215));
+        pbdiviser->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbdiviser, 0, 3, 1, 1);
+
+        pbmultiplier = new QPushButton(verticalLayoutWidget);
+        pbmultiplier->setObjectName(QStringLiteral("pbmultiplier"));
+        pbmultiplier->setMinimumSize(QSize(0, 40));
+        pbmultiplier->setMaximumSize(QSize(40, 16777215));
+        pbmultiplier->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbmultiplier, 1, 3, 1, 1);
+
+        pbmoins = new QPushButton(verticalLayoutWidget);
+        pbmoins->setObjectName(QStringLiteral("pbmoins"));
+        pbmoins->setMinimumSize(QSize(0, 40));
+        pbmoins->setMaximumSize(QSize(40, 16777215));
+        pbmoins->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbmoins, 2, 3, 1, 1);
+
+        pbplus = new QPushButton(verticalLayoutWidget);
+        pbplus->setObjectName(QStringLiteral("pbplus"));
+        pbplus->setMinimumSize(QSize(0, 40));
+        pbplus->setMaximumSize(QSize(40, 16777215));
+        pbplus->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(pbplus, 3, 3, 1, 1);
+
+        Pbegale = new QPushButton(verticalLayoutWidget);
+        Pbegale->setObjectName(QStringLiteral("Pbegale"));
+        Pbegale->setMinimumSize(QSize(0, 40));
+        Pbegale->setMaximumSize(QSize(40, 16777215));
+        Pbegale->setStyleSheet(QStringLiteral("background-color: rgb(255, 170, 0);"));
+
+        gridLayout->addWidget(Pbegale, 4, 3, 1, 1);
+
+
+        verticalLayout->addLayout(gridLayout);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Calculatrice", 0));
+        res->setText(QApplication::translate("Widget", "0", 0));
+        pbquatre->setText(QApplication::translate("Widget", "4", 0));
+        pbsquare->setText(QApplication::translate("Widget", "X\302\262", 0));
+        pbcinq->setText(QApplication::translate("Widget", "5", 0));
+        pbun->setText(QApplication::translate("Widget", "1", 0));
+        Pbplusmoins->setText(QApplication::translate("Widget", "+/-", 0));
+        pbsix->setText(QApplication::translate("Widget", "6", 0));
+        Pbvirgule->setText(QApplication::translate("Widget", ",", 0));
+        Pbzero->setText(QApplication::translate("Widget", "0", 0));
+        pbtrois->setText(QApplication::translate("Widget", "3", 0));
+        pbdeux->setText(QApplication::translate("Widget", "2", 0));
+        pbsept->setText(QApplication::translate("Widget", "7", 0));
+        pbhuit->setText(QApplication::translate("Widget", "8", 0));
+        pbrecule->setText(QApplication::translate("Widget", "<", 0));
+        pbneuf->setText(QApplication::translate("Widget", "9", 0));
+        pbefface->setText(QApplication::translate("Widget", "C", 0));
+        pbdiviser->setText(QApplication::translate("Widget", "%", 0));
+        pbmultiplier->setText(QApplication::translate("Widget", "x", 0));
+        pbmoins->setText(QApplication::translate("Widget", "-", 0));
+        pbplus->setText(QApplication::translate("Widget", "+", 0));
+        Pbegale->setText(QApplication::translate("Widget", "=", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
